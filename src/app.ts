5188172e4ded78dd5fee552a677c0c7210f3bde0
Fergus Lai <ferguslai724@gmail.com>
2025-06-01 20:32:28 +0100

    Check Directory

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,38 @@
 import express from "express";
-const app = express();
-const port = 3000;
+import { directoryTree } from "./data";
+import cors from "cors";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const corsOptions = {
+  origin: process.env.NODE_ENV == "production" ? "https://fergus-lai.dev" : "*",
+  methods: ["GET", "POST"],
+};
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors(corsOptions));
+
+app.get("/checkDirectory/:path(*)", (req, res) => {
+  const path = req.params.path;
+  if (path == "") {
+    res.send(false);
+    return;
+  }
+  if (path == "~") {
+    res.send(true);
+    return;
+  }
+  const directories = path.split("/");
+  let currentDirectory = directoryTree;
+  directories.forEach((directory) => {
+    if (!(directory in currentDirectory)) {
+      res.send(false);
+      return;
+    }
+    currentDirectory = currentDirectory[directory];
+  });
+  res.send(true);
+  return;
 });
 
 app.listen(port, () => {
---
+++ src/data.ts
@@ -0,0 +1,11 @@
+const projects = {};
+
+const experience = {};
+
+const comment = {};
+
+export const directoryTree = {
+  projects: projects,
+  experience: experience,
+  comment: comment,
+};
