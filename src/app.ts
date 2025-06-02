77f3b627310fc22718b7b43151e02c73cdc961b7
Fergus Lai <ferguslai724@gmail.com>
2025-06-02 01:02:22 +0100

    Use Serverless

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { directoryTree } from "./data";
 import cors from "cors";
+import serverless from "serverless-http";
 
 const corsOptions = {
   origin:
@@ -9,7 +10,6 @@ const corsOptions = {
 };
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors(corsOptions));
 
@@ -55,6 +55,4 @@ app.get("/listDirectory/:path(*)", (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+module.exports.handler = serverless(app);
